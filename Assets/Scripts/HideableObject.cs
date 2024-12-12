0b9a2df90bdf97af612d221812939a410b31b0a4
Linea Arnesen <lil.arnesen@gmail.com>
2024-12-12 21:30:44 +0100

    Hideable error fixed

--- Assets/Scripts/HideableObject.cs
+++ Assets/Scripts/HideableObject.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class HideableObject : MonoBehaviour
 {
+    public bool isPlayerInTrigger = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+            Debug.Log("Player entered hideable trigger");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            Debug.Log("Player exited hideable trigger");
+        }
+    }
+    // en bool chekker true or false, og jeg vil gerne tjekke om player er inde i triggeren eller ej, hvilket vil sige om hideable object trigger er til eller ej
     // Start is called before the first frame update
     void Start()
     {
